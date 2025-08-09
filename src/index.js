1bb0b8ff45d4fe80b877d4038ca8348109ade545
Rayees Alam <122892065+Rayeesgithub@users.noreply.github.com>
2025-08-09 18:27:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 
 const app=express();
 
+const cors=require("cors");
 app.use(cors({
   origin: [
     "https://dev-hub-henna.vercel.app", // production
